66790811ff45fbfb95bdd47d143f0add344214aa
Baker, Jake <Jake.Baker+Barco@barco.com>
2024-05-15 08:27:35 -0700

    init jekyll

--- README.md
+++
@@ -1,1 +0,0 @@
-# jakeyeatsteaky.github.io
--- script.js
+++
@@ -1,5 +0,0 @@
-// Add your JavaScript code here
-// For example, you can interact with the DOM or create event listeners
-document.addEventListener('DOMContentLoaded', function() {
-  // DOM elements can be accessed here
-});
